d113e13de3239e01366812b14d7a80877b0edbe7
Thomas Reeves <treeves@northislandcollege.ca>
2017-10-31 21:50:13 -0700

    added php changing excerpt text

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,4 +1,13 @@
 <?php
+
+// changes text of excerpt
+function modify_read_more_link() {
+    return '<a class="more-link" href="' . get_permalink() . '">By Ogdens Hammer what savings!</a>';
+}
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
+
+
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
