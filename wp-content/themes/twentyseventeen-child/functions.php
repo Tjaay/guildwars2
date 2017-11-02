ffdb9df809e7936a71c937a7066240fefef4774f
Thomas Reeves <treeves@northislandcollege.ca>
2017-11-01 23:27:09 -0700

    updated background of site and css

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -3,10 +3,20 @@
 
 // changes text of excerpt read more
 function modify_read_more_link() {
-    return '<a class="more-link" href="' . get_permalink() . '">By Ogdens Hammer what savings!</a>';
+    return '<a class="more-link" href="' . get_permalink() . '">More Shinies!</a>';
 }
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
 
+// adds twitter and facebook to contact methods of users
+function wpb_new_contactmethods( $contactmethods ) {
+    // Add Twitter
+    $contactmethods['twitter'] = 'Twitter';
+    //add Facebook
+    $contactmethods['facebook'] = 'Facebook';
+     
+    return $contactmethods;
+    }
+    add_filter('user_contactmethods','wpb_new_contactmethods',10,1);
 
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
