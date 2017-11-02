9b12d1a6714bf63b119dbc9f40ea1430427b40c3
Thomas Reeves <treeves@northislandcollege.ca>
2017-11-02 00:17:42 -0700

    animate plugin added and copyright date

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -7,6 +7,29 @@ function modify_read_more_link() {
 }
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
 
+// add dynamic copyright date
+function comicpress_copyright() {
+    global $wpdb;
+    $copyright_dates = $wpdb->get_results("
+    SELECT
+    YEAR(min(post_date_gmt)) AS firstdate,
+    YEAR(max(post_date_gmt)) AS lastdate
+    FROM
+    $wpdb->posts
+    WHERE
+    post_status = 'publish'
+    ");
+    $output = '';
+    if($copyright_dates) {
+    $copyright = "&copy; " . $copyright_dates[0]->firstdate;
+    if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+    $copyright .= '-' . $copyright_dates[0]->lastdate;
+    }
+    $output = $copyright;
+    }
+    return $output;
+    }
+
 // adds twitter and facebook to contact methods of users
 function wpb_new_contactmethods( $contactmethods ) {
     // Add Twitter
